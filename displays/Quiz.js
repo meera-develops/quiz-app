6d320b45f9b1e2fa67b8b158b4b34fadf8d9d17d
Meera Bhola <meerabhola3@gmail.com>
2025-04-07 18:12:24 -0400

    set up stack navigator

---
+++ displays/Quiz.js
@@ -0,0 +1,9 @@
+import {Text, View, StyleSheet } from 'react-native';
+
+export default function Quiz({ navigation }) {
+    return (
+        <View>
+            <Text>Welcome to my app</Text>
+        </View>
+    );
+}
